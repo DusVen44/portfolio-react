193e05d13c9c87cabb99de08e62e7d6c2e159b3a
Dustin Venable <Venny44@yahoo.com>
2021-09-20 07:18:01 -0500

    Update Bio

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -11,19 +11,19 @@ export default function Bio() {
             <h4 className="bio-title title">Bio</h4>
             
             <p className="bio-text text1">
-                I am a Cloud Engineer & Full-Stack Web Developer who graduated from Bloc's Remote Web Developer Track program. 
-                I have the AWS Solutions Architect - Associate and the AWS SysOps Administrator - Associate certifications.
+                I am a Cloud Engineer & Full-Stack Web Developer who graduated from Thinkful's Remote Web Developer Track program. 
+                I have the 3 AWS associate-level certifications (see below).
                 My interest in tech stems from a love of making creative ideas
                 come to fruition. I enjoy chasing the feeling of satisfaction that's provided when 
                 the final product comes together, and I love working with a team of 
-                programmers to not only showcase my skills, but also learn from the experts in this industry.
+                programmers to not only showcase my skills, but also to learn from the experts in this industry.
             </p>
             <p className="bio-text text2">
                 In my free time, I'm usually playing guitar, drums, piano, bass guitar, and the 
                 Otamatone (it's like a saxophone mixed with a bagpipe that looks like a Blobfish).
                 I can be found in the gym 4-6 days a week, and I have an extreme passion for exotic
                 sports cars. I also have an unsuccessful YouTube channel where I cover my favorite 
-                songs, and I'm extremely proud of acquiring my 70 subscribers. Check out my portfolio 
+                songs, and I'm extremely proud of acquiring my 69 subscribers. Check out my portfolio 
                 of recent projects, and feel free to contact me.
             </p>
 
